07ad1eba6fe59315ffe2ed80ab49cf11fc452f5a
maya <amacrk@i.softbank.jp>
2020-05-12 00:06:49 +0900

    add 0~9, clear

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,12 +14,21 @@ class ViewController: UIViewController {
     
     var number1: Int = 0
     var number2: Int = 0
-    var number3: Int = 0
     
     var ope: Int = 0  //どの四則演算を判定
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func select1() {
+        number1 = number1 * 10 + 1
+        label.text = String(number1)
+    }
+    
+    @IBAction func select2() {
+        number1 = number1 * 10 + 2
+        label.text = String(number1)
     }
     
     @IBAction func select3() {
@@ -29,6 +38,36 @@ class ViewController: UIViewController {
     
     @IBAction func select4() {
         number1 = number1 * 10 + 4
+        label.text = String(number1)
+    }
+    
+    @IBAction func select5() {
+        number1 = number1 * 10 + 5
+        label.text = String(number1)
+    }
+    
+    @IBAction func select6() {
+        number1 = number1 * 10 + 6
+        label.text = String(number1)
+    }
+    
+    @IBAction func select7() {
+        number1 = number1 * 10 + 7
+        label.text = String(number1)
+    }
+    
+    @IBAction func select8() {
+        number1 = number1 * 10 + 8
+        label.text = String(number1)
+    }
+    
+    @IBAction func select9() {
+        number1 = number1 * 10 + 9
+        label.text = String(number1)
+    }
+    
+    @IBAction func select0() {
+        number1 = number1 * 10 + 0
         label.text = String(number1)
     }
     
@@ -56,21 +95,26 @@ class ViewController: UIViewController {
         ope = 4
     }
     
+    @IBAction func clear() {
+        number1 = 0
+        label.text = String(number1)
+    }
+    
     @IBAction func equal() {
         if ope == 1 {
-            number3 = number2 + number1
+            number1 = number2 + number1
             // plus
         } else if ope == 2 {
-            number3 = number2 - number1
+            number1 = number2 - number1
             // minus
         } else if ope == 3 {
-            number3 = number2 * number1
+            number1 = number2 * number1
             // mul
         } else if ope == 4 {
-            number3 = number2 / number1
+            number1 = number2 / number1
             // div
         }
-        label.text = String(number3)
+        label.text = String(number1)
     }
 
 
